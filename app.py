15619f0cc48ff72232ddf8926ce25bf1d97ccdd3
jeongminju0815 <jmju0815@gmail.com>
2022-11-09 04:25:09 +0000

    prototype

---
+++ app.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+st.set_page_config(page_title="Movie Recommender", layout="wide")
+
+#############################################################
+###                  Define Side-bar View                 ###
+#############################################################
+st.sidebar.title("Setting")
+
+#############################################################
+###                    Define Main View                   ###
+#############################################################
+
+st.title("books Recomender with Streamlit")
